017483cd21170f2d860644bb3f721d9bfb0e5bc4
Ling Shen <ShenPublic@foxmail.com>
2024-08-12 12:17:46 +0800

    *  add custom accumulator demo

--- src/main/scala/com/ds/other/AccumulatorDemo.scala
+++ src/main/scala/com/ds/other/AccumulatorDemo.scala
@@ -5,7 +5,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object AccumulatorDemo {
   def main(args: Array[String]): Unit = {
     // 累加器变量  driver端可读  executor可写 只能累加 不可读
-    val sc = SparkContext.getOrCreate(new SparkConf().setMaster("local[*]").setAppName("broadcastTest"))
+    val sc = SparkContext.getOrCreate(new SparkConf().setMaster("local[*]").setAppName("accumulator demo"))
     // Spark 提供了三种累加器
     // 对于整型数值累加就采用 longAccumulator
     // 浮点型数值累加采用 doubleAccumulator(整型数值都可以转化成浮 点型数值 所以 doubleAccumulator 也可以进行整型数值的累加)
